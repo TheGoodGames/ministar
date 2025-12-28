a4b75a112b44b9d56f9b2a73aaea3a4038969f91
Leonid <108025594+TheGoodGames@users.noreply.github.com>
2025-12-28 07:09:09 +0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// ⚠️ НИКОГДА НЕ КОММИТЬТЕ ЭТОТ ФАЙЛ В GIT!
-window.GAME_CONFIG = {
-  BOT_TOKEN: 'ВАШ_BOT_TOKEN_ЗДЕСЬ', // Пример: '123456789:AAHjK7vZabc123def456ghi789'
-  PUBLIC_CHAT_ID: '@lin_drake_game_bot' // Ваш username бота с @
-};
